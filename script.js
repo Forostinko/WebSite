9fd6f44498517a03df2ef71add8a8131c3802dc0
Olga Forostinko <oforostinko@gmail.com>
2023-05-18 12:03:34 -0700

    added burger menu

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
+// Burger
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+    document.querySelector(".container-header").classList.toggle("open")
+  })
+})
+
+// Close on click Esc
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    document.querySelector(".container-header").classList.remove("open")
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ==============================   Burger    =========================================
 
-const hamb = document.querySelector("#hamburger");
-const popup = document.querySelector("#popup");
-const body = document.body;
+// const hamb = document.querySelector("#hamburger");
+// const popup = document.querySelector("#popup");
+// const body = document.body;
 
-// Клонируем меню, чтобы задать свои стили для мобильной версии
-const menu = document.querySelector("#menu").cloneNode(1);
+// // Клонируем меню, чтобы задать свои стили для мобильной версии
+// const menu = document.querySelector("#menu").cloneNode(1);
 
-// При клике на иконку hamb вызываем ф-ию hambHandler
-hamb.addEventListener("click", hambHandler);
+// // При клике на иконку hamb вызываем ф-ию hambHandler
+// hamb.addEventListener("click", hambHandler);
 
-// Выполняем действия при клике ..
-function hambHandler(e) {
-  e.preventDefault();
-  // Переключаем стили элементов при клике
-  popup.classList.toggle("open");
-  hamb.classList.toggle("active");
-  body.classList.toggle("noscroll");
-  renderPopup();
-}
+// // Выполняем действия при клике ..
+// function hambHandler(e) {
+//   e.preventDefault();
+//   // Переключаем стили элементов при клике
+//   popup.classList.toggle("open");
+//   hamb.classList.toggle("active");
+//   body.classList.toggle("noscroll");
+//   renderPopup();
+// }
 
-// Здесь мы рендерим элементы в наш попап
-function renderPopup() {
-  popup.appendChild(menu);
-}
+// // Здесь мы рендерим элементы в наш попап
+// function renderPopup() {
+//   popup.appendChild(menu);
+// }
 
-// Код для закрытия меню при нажатии на ссылку
-const links = Array.from(menu.children);
+// // Код для закрытия меню при нажатии на ссылку
+// const links = Array.from(menu.children);
 
-// Для каждого элемента меню при клике вызываем ф-ию
-links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
-});
+// // Для каждого элемента меню при клике вызываем ф-ию
+// links.forEach((link) => {
+//   link.addEventListener("click", closeOnClick);
+// });
 
-// Закрытие попапа при клике на меню
-function closeOnClick() {
-  popup.classList.remove("open");
-  hamb.classList.remove("active");
-  body.classList.remove("noscroll");
-}
+// // Закрытие попапа при клике на меню
+// function closeOnClick() {
+//   popup.classList.remove("open");
+//   hamb.classList.remove("active");
+//   body.classList.remove("noscroll");
+// }
 
 
 // ==============================   Slider   ============================================
